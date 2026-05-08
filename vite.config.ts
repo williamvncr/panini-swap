e90b1ca088a42cf9e673e72ce081028f4ebc9b5f
williamvncr <williamvn@gmail.com>
2026-05-08 17:29:11 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: []
-  },
   build: {
     rollupOptions: {
       external: [
         'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js',
         'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js'
-      ]
+      ],
+      output: {
+        paths: {
+          'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js': 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js',
+          'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js': 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js'
+        }
+      }
     }
   }
 })
